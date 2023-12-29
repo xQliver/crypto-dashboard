142965a887e0fa43e611f1a1007edf05f1bc1a53
xQliver <oli_48@hotmail.com>
2023-12-29 21:13:50 +0100

    Added Bitcoin dominance
    Bitcoin dominance added to dashboard

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ BITCOIN_API_URL = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_
 BITCOIN_GAS_API_URL = "https://mempool.space/api/v1/fees/mempool-blocks"
 ETHEREUM_API_URL = "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd&include_market_cap=false&include_24hr_vol=false&include_24hr_change=true&include_last_updated_at=false"
 ETHEREUM_GAS_API_URL = "https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=761AJ5NJ93VZPZ96PFD3QBWBMJMEAZ5SB5"
+BITCOIN_DOMINANCE_API_URL = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=125&page=1&sparkline=false"
 FEAR_GREED_API_URL = "https://api.alternative.me/fng/"
 
 
@@ -56,6 +57,32 @@ def ethereum_gas_price():
         return jsonify(response.json())
     except requests.exceptions.RequestException as e:
         print(f"Request to {ETHEREUM_GAS_API_URL} failed: {e}")
+        return jsonify({"error": "Unable to fetch data"}), 500
+
+
+@app.route("/btc_dominance")
+def btc_dominance():
+    try:
+        response = requests.get(BITCOIN_DOMINANCE_API_URL, timeout=60)
+        response.raise_for_status()
+
+        # Parse the JSON content from the response
+        data = response.json()
+
+        # Initialize altCap
+        altCap = 0
+
+        # Iterate through the parsed data
+        for x in data:
+            if x["id"] == "bitcoin":
+                BTCCap = x["market_cap"]
+            altCap += x["market_cap"]
+
+        dominance = (BTCCap / altCap) * 100
+        return jsonify({"data": [{"value": dominance}]})
+
+    except requests.exceptions.RequestException as e:
+        print(f"Request to {BITCOIN_DOMINANCE_API_URL} failed: {e}")
         return jsonify({"error": "Unable to fetch data"}), 500
 
 
